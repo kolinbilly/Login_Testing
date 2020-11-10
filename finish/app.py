f5522864f31350d957eeb6603c84c8b93f381b80
DESKTOP-TBBDDJ1\Cyber <kolinszeto@gmail.com>
2020-11-10 13:09:53 -0800

    got all the downloads completed

--- finish/app.py
+++ finish/app.py
@@ -3,9 +3,10 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from sqlite3 import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
